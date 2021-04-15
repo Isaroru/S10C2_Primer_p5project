2a61b59de76b5b8d38f1d81e3e907506744fc7a9
Isaroru <isabellaroru@hotmail.com>
2021-04-15 09:47:05 -0500

    Primer commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(242,70,98);
+  fill(255);
+  textSize(50);
+  textAlign(CENTER);
+  text("¡Hola Mundo! <3",200,200);
+}
